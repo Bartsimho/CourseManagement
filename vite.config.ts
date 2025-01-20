9b971844d2f3299316c1ebdc06ab253ce045cae5
Bartsimho <56699134+Bartsimho@users.noreply.github.com>
2025-01-20 13:26:47 +0000

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Ensure Vite is correctly splitting and handling modules
+        },
+      },
+    },
   },
 });
